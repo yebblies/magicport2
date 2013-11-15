e15953d00e3d363ea7c0cb0057eeed1b461acf14
Daniel Murphy <yebblies@gmail.com>
2013-11-15 17:28:53 +1100

    Explicit .ptr

--- defs.d
+++ defs.d
@@ -827,13 +827,13 @@ size_t ld_sprint(char* str, int fmt, real x)
         // see http://en.cppreference.com/w/cpp/io/c/fprintf
         char sfmt[5] = "%#Lg\0";
         sfmt[3] = cast(char)fmt;
-        return sprintf(str, sfmt, x);
+        return sprintf(str, sfmt.ptr, x);
     }
     else
     {
         char sfmt[4] = "%Lg\0";
         sfmt[2] = cast(char)fmt;
-        return sprintf(str, sfmt, x);
+        return sprintf(str, sfmt.ptr, x);
     }
 }
 
