5de39162902055a8ee2473e53fc119632d9ee1e4
Daniel Murphy <yebblies@gmail.com>
2014-02-23 15:55:53 +1100

    Import core.stdc.limits in the modules that need it

--- defs.d
+++ defs.d
@@ -1,7 +1,6 @@
 
 // c library
 
-public import core.stdc.limits : INT_MAX;
 public import core.stdc.math : sinl, cosl, tanl, sqrtl, fabsl;
 public import core.stdc.time : time_t, ctime, time;
 public import core.stdc.stdint : int64_t, uint64_t, int32_t, uint32_t, int16_t, uint16_t, int8_t, uint8_t;
